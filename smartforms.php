e66e46012bd0612f815c6d8d434e4155054e417c
EDGARROJAS <EDGARROJAS@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-26 18:42:08 +0000

    Fixing issue with form list.
    git-svn-id: https://plugins.svn.wordpress.org/smart-forms/trunk@939239 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- smartforms.php
+++ smartforms.php
@@ -5,7 +5,7 @@
  * Description: Place diferent form of donations on your blog...
  * Author: RedNao
  * Author URI: http://rednao.com
- * Version: 1.5.23
+ * Version: 1.5.24
  * Text Domain: SmartDonations
  * Domain Path: /languages/
  * Network: true
