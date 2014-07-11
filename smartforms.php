a379a109213623bfd083d8edf68cbd6c6704c888
EDGARROJAS <EDGARROJAS@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-11 13:27:24 +0000

    Please check change log to see the changes
    git-svn-id: https://plugins.svn.wordpress.org/smart-forms/trunk@946890 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- smartforms.php
+++ smartforms.php
@@ -5,7 +5,7 @@
  * Description: Place diferent form of donations on your blog...
  * Author: RedNao
  * Author URI: http://rednao.com
- * Version: 2.0
+ * Version: 2.0.1
  * Text Domain: SmartDonations
  * Domain Path: /languages/
  * Network: true
