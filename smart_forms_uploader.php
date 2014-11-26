81e795403470aa75113436f1d573a36c4ecf0926
EDGARROJAS <EDGARROJAS@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-26 02:32:45 +0000

    Please check change log to see the changes
    git-svn-id: https://plugins.svn.wordpress.org/smart-forms/trunk@1032874 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- smart_forms_uploader.php
+++ smart_forms_uploader.php
@@ -10,7 +10,10 @@ if(isset($_POST["data"]))
 $data=json_decode($data,true);
 foreach($data as $key=>$value)
 {
-	$_POST[$key]=addslashes($value);
+	if($key=='captcha')
+		$_POST[$key]=$value;
+	else
+		$_POST[$key]=addslashes($value);
 }
 require_once "smart-forms-ajax.php";
 rednao_smart_forms_save_form_values();
