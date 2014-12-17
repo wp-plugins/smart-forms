a2407c79e59ff244bcc8cba3beb7e0a1da88a382
EDGARROJAS <EDGARROJAS@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-17 21:18:22 +0000

    please check change log
    git-svn-id: https://plugins.svn.wordpress.org/smart-forms/trunk@1047572 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/formBuilder/properties/manipulators.js
+++ js/formBuilder/properties/manipulators.js
@@ -13,7 +13,7 @@ RedNaoBasicManipulator.prototype.GetValue=function(propertiesObject,propertyName
 RedNaoBasicManipulator.prototype.SetValue=function(propertiesObject,propertyName,value,additionalInformation)
 {
     propertiesObject[propertyName]=value;
-    if(typeof additionalInformation.ChangeCallBack!='undefined')
+    if (typeof additionalInformation != 'undefined' && typeof additionalInformation.ChangeCallBack != 'undefined')
         additionalInformation.ChangeCallBack(value);
 };
 
